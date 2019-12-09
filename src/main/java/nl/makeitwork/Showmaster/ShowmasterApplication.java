4e39cbc0142859d236130ebae70bf68433daf202
GGPostma <41754176+GGPostma@users.noreply.github.com>
2019-12-09 15:39:28 +0100

    Initial commit

---
+++ src/main/java/nl/makeitwork/Showmaster/ShowmasterApplication.java
@@ -0,0 +1,13 @@
+package nl.makeitwork.Showmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/makeitwork/Showmaster/ShowmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.makeitwork.Showmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
