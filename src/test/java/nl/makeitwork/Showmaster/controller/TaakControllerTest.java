971dc18ddb38f4c7cdcb93e86888cfcda1364737
Karin Zoetendal <kezoetendal@hotmail.com>
2019-12-11 16:35:55 +0100

    TaakControllerTest opzet gemaakt, moet nog worden uitgewerkt. Test properties in resources map aangemaakt, zodat getest wordt met showmastertest-database

---
+++ src/test/java/nl/makeitwork/Showmaster/controller/TaakControllerTest.java
@@ -0,0 +1,19 @@
+package nl.makeitwork.Showmaster.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestPropertySource(locations = "classpath:test.properties")
+class TaakControllerTest {
+
+
+    @Test
+    void showTaakAanmaken() {
+    }
+
+    @Test
+    void saveTaakAanmaken() {
+    }
+}
