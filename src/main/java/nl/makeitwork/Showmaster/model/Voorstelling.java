d3fa48c89d088d1087fb2781fbd8aa3e7a583304
Pieter Dijkema <pieter_dijkema@hotmail.com>
2019-12-11 13:38:37 +0100

    naamgeving gerefactored

---
+++ src/main/java/nl/makeitwork/Showmaster/model/Voorstelling.java
@@ -0,0 +1,42 @@
+package nl.makeitwork.Showmaster.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Voorstelling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer voorstellingId;
+    private String naam;
+    private Date datum = new Date();
+
+    public Integer getVoorstellingId() {
+        return voorstellingId;
+    }
+
+    public void setVoorstellingId(Integer voorstellingId) {
+        this.voorstellingId = voorstellingId;
+    }
+
+    public String getName() {
+        return naam;
+    }
+
+
+    public void setName(String name) {
+        this.naam = name;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+}
