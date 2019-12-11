63031265c1b718adcb3d65ce60e018e4d002d26d
Pieter Dijkema <pieter_dijkema@hotmail.com>
2019-12-11 13:28:30 +0100

    Class ShowMasterShow aangemaakt

--- src/main/java/nl/makeitwork/Showmaster/model/ShowMasterShow.java
+++ src/main/java/nl/makeitwork/Showmaster/model/ShowMasterShow.java
@@ -1,4 +1,41 @@
 package nl.makeitwork.Showmaster.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class ShowMasterShow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer showId;
+    private String name;
+    private Date date = new Date();
+
+    public Integer getShowId() {
+        return showId;
+    }
+
+    public void setShowId(Integer showId) {
+        this.showId = showId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
