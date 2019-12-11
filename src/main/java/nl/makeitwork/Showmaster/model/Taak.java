750defb5a4d9e4ceaf2ebb50670f5f9f97ef7c81
Karin Zoetendal <kezoetendal@hotmail.com>
2019-12-11 14:04:30 +0100

    Class Taak aangemaakt, Use case diagram en ERD toegevoegd

---
+++ src/main/java/nl/makeitwork/Showmaster/model/Taak.java
@@ -0,0 +1,44 @@
+package nl.makeitwork.Showmaster.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Taak {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer taakId;
+
+    private String taakNaam;
+
+    private Integer standaardBezetting;
+
+    public Taak() {}
+
+    public Integer getTaakId() {
+        return taakId;
+    }
+
+    public void setTaakId(Integer taakId) {
+        this.taakId = taakId;
+    }
+
+    public String getTaakNaam() {
+        return taakNaam;
+    }
+
+    public void setTaakNaam(String taakNaam) {
+        this.taakNaam = taakNaam;
+    }
+
+    public Integer getStandaardBezetting() {
+        return standaardBezetting;
+    }
+
+    public void setStandaardBezetting(Integer standaardBezetting) {
+        this.standaardBezetting = standaardBezetting;
+    }
+}
