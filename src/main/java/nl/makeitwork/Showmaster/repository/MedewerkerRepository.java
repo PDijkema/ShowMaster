07bb1fe90be55112e45acded29636f8051403d19
Karin Zoetendal <kezoetendal@hotmail.com>
2019-12-12 11:31:19 +0100

    MedewerkerRepository teruggezet naar beginwaarden

--- src/main/java/nl/makeitwork/Showmaster/repository/MedewerkerRepository.java
+++ src/main/java/nl/makeitwork/Showmaster/repository/MedewerkerRepository.java
@@ -1,8 +1,6 @@
-/*
+
 package nl.makeitwork.Showmaster.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedewerkerRepository extends JpaRepository {
+public interface MedewerkerRepository {
 }
-*/
+
