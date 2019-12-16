766edbe9894a518cef336c43295f61093e73cab4
Pieter Dijkema <pieter_dijkema@hotmail.com>
2019-12-16 16:06:38 +0100

    Unittest voor verwijderen voorstelling toegevoegd

--- src/test/java/nl/makeitwork/Showmaster/controller/VoorstellingControllerTest.java
+++ src/test/java/nl/makeitwork/Showmaster/controller/VoorstellingControllerTest.java
@@ -9,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.LocalDateTime;
 import java.time.Month;
+
+import static org.mockito.Mockito.*;
 
 
 @RunWith(SpringRunner.class)
@@ -28,21 +32,40 @@ class VoorstellingControllerTest {
     void saveOrUpdateVoorstelling() {
 
         //arrange
-        Voorstelling voorstelling2 = new Voorstelling();
+        Voorstelling voorstelling = new Voorstelling();
         LocalDateTime datum = LocalDateTime.of(2020, Month.OCTOBER, 10, 20, 30);
-        voorstelling2.setNaam("Lion King");
-        voorstelling2.setDatum(datum);
+        voorstelling.setNaam("Lion King");
+        voorstelling.setDatum(datum);
 
         //activate
-        voorstellingController.saveOrUpdateVoorstelling(voorstelling2);
+        voorstellingController.saveOrUpdateVoorstelling(voorstelling);
 
         //assert
-        Assert.assertNotNull(voorstellingRepository.findById(voorstelling2.getVoorstellingId()));
+        Assert.assertNotNull(voorstellingRepository.findById(voorstelling.getVoorstellingId()));
     }
 
     @Test
     void alleVoorstellingen() {
         Assert.assertNotNull(voorstellingController);
+
+    }
+
+
+    @Test
+    void verwijderVoorstelling() {
+
+        //arrange
+        Voorstelling voorstelling = new Voorstelling();
+        LocalDateTime datum = LocalDateTime.of(2020, Month.JANUARY, 18, 20, 30);
+        voorstelling.setNaam("Soldaat van Oranje");
+        voorstelling.setDatum(datum);
+
+        //activate
+        voorstellingController.saveOrUpdateVoorstelling(voorstelling);
+        voorstellingRepository.delete(voorstelling);
+
+        //assert
+        Assert.assertFalse(voorstellingRepository.existsById(voorstelling.getVoorstellingId()));
 
     }
 }
