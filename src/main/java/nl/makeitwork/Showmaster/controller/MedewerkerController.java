21516298886488aa1dac70091da0cb2297d5081e
Karin Zoetendal <kezoetendal@hotmail.com>
2019-12-17 11:17:34 +0100

    profielPagina heet nu profielWijzigen. Een HTML_test toegevoegd om jsp code te checken (sluit tags etc). In MedewerkerController mapping voor profielWijzigen toegevoegd

--- src/main/java/nl/makeitwork/Showmaster/controller/MedewerkerController.java
+++ src/main/java/nl/makeitwork/Showmaster/controller/MedewerkerController.java
@@ -2,6 +2,7 @@ package nl.makeitwork.Showmaster.controller;
 
 import nl.makeitwork.Showmaster.model.Medewerker;
 import nl.makeitwork.Showmaster.repository.MedewerkerRepository;
+import nl.makeitwork.Showmaster.repository.TaakRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -14,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @Author Gert Postma
+ * 17-12-19 - Karin Zoetendal: profiel/wijzigen get en postmapping toegevoegd, moet nog aangepost worden
  */
 @Controller
 public class MedewerkerController {
 
     @Autowired
     MedewerkerRepository medewerkerRepository;
+
+    @Autowired
+    TaakRepository taakRepository;
 
     @GetMapping("/registreer")
     protected String showRegistratieFormulier(Model model) {
@@ -33,12 +38,26 @@ public class MedewerkerController {
         return "redirect:/registreer";
     }
 
-    @GetMapping("/profielpagina")
+    // moet wellicht aangepast, moet medewerkerId meegeven
+    @GetMapping("/profiel/wijzigen")
     protected String showProfielpagina(Model model) {
-        model.addAttribute("profielPagina", new Medewerker());
-        return "profielPagina";
+        model.addAttribute("medewerker", new Medewerker());
+        model.addAttribute("takenLijst", taakRepository.findAll());
+        return "profielWijzigen";
     }
 
+
+    // Redirect moet nog worden aangepast, moet terug naar profielPagina (overzicht profielgegevens)
+    @PostMapping("/profiel/wijzigen")
+    public String saveOrUpdateGebruiker (@ModelAttribute("profielWijzigen") Medewerker profielWijzigingen,
+                                         BindingResult result) {
+        if (result.hasErrors()) {
+            return "profielWijzigen";
+        } else {
+            medewerkerRepository.save(profielWijzigingen);
+            return "redirect:/takenlijst";
+        }
+    }
 
     }
 
