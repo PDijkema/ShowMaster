d098107ae2e1507d539cd48019169acef36a49d4
Pieter Dijkema <pieter_dijkema@hotmail.com>
2020-01-08 11:36:31 +0100

    toevoegen diensten bij voorstelling nu mogelijk

--- src/main/java/nl/makeitwork/Showmaster/controller/VoorstellingsTaakController.java
+++ src/main/java/nl/makeitwork/Showmaster/controller/VoorstellingsTaakController.java
@@ -35,30 +35,31 @@ public class VoorstellingsTaakController {
     private TaakRepository taakRepository;
     @Autowired
     private VoorstellingsTaakRepository voorstellingsTaakRepository;
-
-/*    @GetMapping("/toevoegenTaken")
-    protected String toevoegenTakenAanVoorstelling(Model model) {
-        model.addAttribute("alleTaken", taakRepository.findAll());
-        return "toevoegenTakenAanVoorstelling";
-    }*/
-
-
+    
     @GetMapping("/voorstellingsTaak/verwijderen/{voorstellingId}/{voorstellingsTaakId}")
-    protected String verwijderenTaakBijVoorstelling(@PathVariable("voorstellingsTaakId") Integer voorstellingsTaakId) {
+    protected String verwijderenTaakBijVoorstelling(@PathVariable("voorstellingsTaakId") Integer voorstellingsTaakId,
+                                                    @PathVariable("voorstellingId") Integer voorstellingId) {
 
         Optional<VoorstellingsTaak> voorstellingsTaak = voorstellingsTaakRepository.findById(voorstellingsTaakId);
-        Integer voorstellingId = voorstellingsTaak.get().getVoorstelling().getVoorstellingId();
-
         voorstellingsTaakRepository.deleteById(voorstellingsTaakId);
 
         return "redirect:/voorstelling/details/" + voorstellingId;
     }
 
     @GetMapping("/voorstellingsTaak/toevoegen/{voorstellingId}/{taakId}")
-    protected String toevoegenTaakAanVoorstelling(Model model) {
-        System.out.println("werkt");
-        model.addAttribute("alleTaken", taakRepository.findAll());
-        return "redirect:/voorstellingen";
+    protected String toevoegenTaakAanVoorstelling(@PathVariable("taakId") Integer taakId,
+                                                  @PathVariable("voorstellingId") Integer voorstellingId) {
+
+        Optional<Voorstelling> voorstelling = voorstellingRepository.findById(voorstellingId);
+        Optional<Taak> taak = taakRepository.findById(taakId);
+
+        VoorstellingsTaak voorstellingsTaak = new VoorstellingsTaak();
+        voorstellingsTaak.setVoorstelling(voorstelling.get());
+        voorstellingsTaak.setTaak(taak.get());
+
+        voorstellingsTaakRepository.save(voorstellingsTaak);
+
+        return "redirect:/voorstelling/details/" + voorstellingId;
     }
 
 
